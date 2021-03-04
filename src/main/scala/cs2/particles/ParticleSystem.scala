eeeb4f892ab1540db79944bd475709c578718e6a
Matthew Hibbs <mhibbs@trinity.edu>
2021-03-04 16:46:26 -0600

    Maps, Buffers, started Stacks

--- src/main/scala/cs2/particles/ParticleSystem.scala
+++ src/main/scala/cs2/particles/ParticleSystem.scala
@@ -2,15 +2,16 @@ package cs2.particles
 
 import cs2.util.Vec2
 import scalafx.scene.canvas.GraphicsContext
+import scala.collection.mutable.Buffer
 
 class ParticleSystem(val origin:Vec2) {
-    protected var particles:List[Particle] = Nil
+    protected var particles:Buffer[Particle] = Buffer()
 
     def addParticle():Unit = {
         if(math.random() < 0.5) {
-            particles ::= new RoundParticle(origin.clone, new Vec2(math.random()*4-2, math.random()*4-2))    
+            particles += new RoundParticle(origin.clone, new Vec2(math.random()*4-2, math.random()*4-2))    
         } else {
-            particles ::= new SquareParticle(Vec2(origin), new Vec2(math.random()*4-2, math.random()*4-2))
+            particles += new SquareParticle(Vec2(origin), new Vec2(math.random()*4-2, math.random()*4-2))
         }
     }
     def display(g:GraphicsContext):Unit = {
@@ -21,7 +22,23 @@ class ParticleSystem(val origin:Vec2) {
     }
     def timeStep():Unit = {
         particles.foreach((p:Particle) => p.timeStep())
-        //particles.foreach(_.timeStep)
+        /* This is bad because you shouldn't change the size of
+           a collection during a for loop over it
+        for(p <- particles) {
+            if(p.isOffScreen) {
+                particles -= p
+            }
+        }*/
+        /* This works, but you may accidentally create an infinite loop
+        var i = 0
+        while(i < particles.length) {
+            if(particles(i).isOffScreen) {
+                particles.remove(i)
+            } else {
+                i += 1
+            }
+        }*/
+        particles = particles.filterNot((p:Particle) => p.isOffScreen)
     }
     def applyForce(force:Vec2):Unit = {
         for(p <- particles) {
--- src/main/scala/cs2/particles/RainbowParticleSystem.scala
+++ src/main/scala/cs2/particles/RainbowParticleSystem.scala
@@ -4,6 +4,6 @@ import cs2.util.Vec2
 
 class RainbowParticleSystem(o:Vec2) extends ParticleSystem(o) {
     override def addParticle(): Unit = {
-        particles ::= new RainbowParticle(origin.clone, new Vec2(math.random()*4-2, math.random()*4-2))
+        particles += new RainbowParticle(origin.clone, new Vec2(math.random()*4-2, math.random()*4-2))
     }
 }

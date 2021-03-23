7db6e756ec1a756f63eb6737c8a9a96e36a37047
Matt Hibbs <mhibbs@trinity.edu>
2021-03-23 15:43:15 -0500

    linked priority queue

--- src/main/scala/cs2/adt/DoubleLinkedSeq.scala
+++ src/main/scala/cs2/adt/DoubleLinkedSeq.scala
@@ -1,76 +1,76 @@
 package cs2.adt
 
-class DoubleLinkedSeq[A : Manifest] extends Seq[A] {
-    private class Node(var data:A, var prev:Node, var next:Node) {
-        def getNode(idx:Int):Node = {
-            if(idx >= 0 && idx < len) {
-                var rover = this
-                for(ctr <- 1 to idx) {
-                    rover = rover.next
-                }
-                rover
-            } else {
-                end
-            }
+class DoubleLinkedSeq[A: Manifest] extends Seq[A] {
+  private class Node(var data: A, var prev: Node, var next: Node) {
+    def getNode(idx: Int): Node = {
+      if (idx >= 0 && idx < len) {
+        var rover = this
+        for (ctr <- 1 to idx) {
+          rover = rover.next
         }
+        rover
+      } else {
+        end
+      }
     }
+  }
 
-    class BidirectionalIterator extends Iterator[A] {
-        private var rover = end.next
-        def next():A = {
-            val ret = rover.data
-            rover = rover.next
-            ret
-        }
-        def hasNext():Boolean = {
-            rover != end
-        }
-        def previous():A = {
-            val ret = rover.data
-            rover = rover.prev
-            ret
-        }
-        def hasPrevious():Boolean = {
-            rover != end
-        }
+  class BidirectionalIterator extends Iterator[A] {
+    private var rover = end.next
+    def next(): A = {
+      val ret = rover.data
+      rover = rover.next
+      ret
     }
+    def hasNext(): Boolean = {
+      rover != end
+    }
+    def previous(): A = {
+      val ret = rover.data
+      rover = rover.prev
+      ret
+    }
+    def hasPrevious(): Boolean = {
+      rover != end
+    }
+  }
 
-    private var end:Node = new Node(new Array[A](1)(0), null, null)
-    end.prev = end
-    end.next = end
-    private var len:Int = 0
+  private var end: Node = new Node(new Array[A](1)(0), null, null)
+  end.prev = end
+  end.next = end
+  private var len: Int = 0
 
-    def iterator():BidirectionalIterator = new BidirectionalIterator()
+  def iterator(): BidirectionalIterator = new BidirectionalIterator()
 
-    def get(idx:Int):A = {
-        val rover = end.next.getNode(idx)
-        rover.data
-    }
-    def set(idx:Int, elem:A):Unit = {
-        val rover = end.next.getNode(idx)
-        rover.data = elem
-    }
-    def insert(idx:Int, elem:A):Unit = {
-        val rover = end.next.getNode(idx - 1)
-        rover.next = new Node(elem, rover, rover.next)
-        rover.next.next.prev = rover.next
-        len += 1
-    }
-    def remove(idx:Int):A = {
-        val rover = end.next.getNode(idx - 1)
-        val ret = rover.next.data
-        rover.next = rover.next.next
-        rover.next.prev = rover
-        len -= 1
-        ret
-    }
-    def length():Int = len
+  def get(idx: Int): A = {
+    val rover = end.next.getNode(idx)
+    rover.data
+  }
+  def set(myIdx: Int, elem: A): Unit = {
+    val rover = end.next.getNode(myIdx)
+    rover.data = elem
+  }
+  def insert(idx: Int, elem: A): Unit = {
+    val rover = end.next.getNode(idx - 1)
+    rover.next = new Node(elem, rover, rover.next)
+    rover.next.next.prev = rover.next
+    len += 1
+  }
+  def remove(idx: Int): A = {
+    val rover = end.next.getNode(idx - 1)
+    val ret = rover.next.data
+    rover.next = rover.next.next
+    rover.next.prev = rover
+    len -= 1
+    ret
+  }
+  def length(): Int = len
 
-    override def append(elem:A):Unit = {
-        val rover = end.prev
-        rover.next = new Node(elem, rover, rover.next)
-        rover.next.next.prev = rover.next
-        len += 1
-    }
+  override def append(elem: A): Unit = {
+    val rover = end.prev
+    rover.next = new Node(elem, rover, rover.next)
+    rover.next.next.prev = rover.next
+    len += 1
+  }
 
 }
---
+++ src/main/scala/cs2/adt/LinkedPriorityQueue.scala
@@ -0,0 +1,63 @@
+package cs2.adt
+
+class LinkedPriorityQueue[A <% Ordered[A]] extends PriorityQueue[A] {
+  private class Node(var data:A, var next:Node)
+  private var head:Node = null
+
+  def add(elem:A):Unit = {
+    if(head == null || elem > head.data) {
+      head = new Node(elem, head)
+    } else {
+      var rover = head
+      while(rover.next != null && rover.next.data > elem) {
+        rover = rover.next
+      }
+      rover.next = new Node(elem, rover.next)
+    }
+
+  }
+  def get():A = {
+    var ret = head.data
+    head = head.next
+    ret
+  }
+  def peek():A = head.data
+  def isEmpty():Boolean = { head == null }
+
+  /* Extra work in get/peek
+  def add(elem:A):Unit = { head = new Node(elem, head) }
+  def get():A = {
+    //Belief values
+    var biggest = head.data
+    var obb:Node = null
+    //Iterating values
+    var rover = head
+    var trail:Node = null
+
+    while(rover != null) {
+      if(rover.data > biggest) {
+        biggest = rover.data
+        obb = trail
+      }
+      trail = rover
+      rover = rover.next
+    }
+    if(obb == null) {
+      head = head.next
+    } else {
+      obb.next = obb.next.next
+    }
+    biggest
+  }
+  def peek():A = {
+    var biggest = head.data
+    var rover = head
+    while(rover != null) {
+      if(rover.data > biggest) biggest = rover.data
+      rover = rover.next
+    }
+    biggest
+  }
+  def isEmpty():Boolean = { head == null }
+  */
+}
